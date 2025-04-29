a4157ab76d39da8c9f516b44c9cac6a8d1d914b3
chinuuuuuu <chinmaydhal6370@gmail.com>
2025-04-30 00:23:58 +0530

    new code

--- understandingConditional/first.js
+++ understandingConditional/first.js
@@ -17,8 +17,119 @@ function checkVoter() {
   }
 
   // shop discount
-  let amount = Number(prompt("Enter amount"));
-  
-  if()
+ 
+
+  //Positive or Negative number
+  function positiveNegetiveNumber (num) {
+    if (num > 0) console.log("the number is possitive");
+    else if (num == 0) console.log("the number is zero");
+    else console.log("the number is negative");
+  };
+
+
+  //Even or Odd number
+  function evenOdd(num) {
+    if(num % 2 === 0 ) console.log("Even num");
+    else console.log("Odd number");
+  }
+
+  // Binary representation 
+
+     function convertToBonary(num) {
+        let binary = " ";
+        if(num === 0) return console.log("binary representation of the num is 0");
+        else {
+            while(num > 0) {
+                let remainder  = num % 2;
+                binary = remainder + binary;
+                num = Math.floor(num/2);
+            }
+            binary = Number(num);
+            return console.log(`binary representation of the num is ${num} `) ;
+        }
+
+     }
+     convertToBonary(10);
+
+     // By using a inbuilt js method 
+     let binary = num => console.log(num.toString(2));
+     binary(10);
+
+
+     //  convert a number By using bit wise operator 
+     let  decimalToBinary = function(num) {
+        if(num === 0)  return console.log(`the binary representation of ${num} is 0`);
+        else{
+            let binary = "";
+
+            while(num > 0) {
+                let lastbit = num & 1;
+                binary =  lastbit + binary;
+                num = num >> 1;
+            }
+
+            return  console.log(binary);
+        }
+     }
+
+     decimalToBinary(10);
+
+
+    //  Check if a number is a multiple of another
+
+    let multiple = (a, b) => {
+        if (a % b === 0) {
+            console.log(`${a} is a multiple of ${b}`);
+        } else {
+            console.log(`${a} is not a multiple of ${b}`);
+        }
+    };
+    
+    multiple(10, 5); 
+    multiple(10, 3); 
+
+    // The sum of the first N natural numbers 
+
+    function sumOfNaturalNumber(num) {
+        let sum = (num * (num + 1))/2
+        return console.log(`sum of num is  ${sum}`);
+    }
+
+    sumOfNaturalNumber(10);
+
+    //Sum of numbers in a given range
+
+    function sumIntRange(start, end) {
+        let sum = 0;
+        for (let i = start; i <= end; i ++) {
+            sum += i;
+        }
+        return console.log(` the sum of given range is ${sum} `);
+    }
+
+    sumIntRange(10, 11);
+
+    //Sum of digits until a single digit (Digital Root)
+
+    function unitDigitOfSum(num) {
+
+        while(num >= 10) {
+            let sum = 0;
+            while(num > 0) {
+                sum += num % 10;
+                num = Math.floor(num / 10);
+            }
+            num = sum;
+        }
+        return console.log(num);
+        
+    }
+    unitDigitOfSum(111);
+
+//
+
+    
+
+
 
   
